c52c5d27697f311c1e1994ed53bc5021755b4bd7
binyamyohannes <olangobinyam@gmail.com>
2024-01-03 00:13:35 +0300

    Add style to the cards

--- src/components/MealCategory.js
+++ src/components/MealCategory.js
@@ -3,15 +3,20 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
+import { Link } from 'react-router-dom';
 
 const MealCategory = ({ mealCategory }) => {
   return (
     mealCategory.categories.map(category => (
       <Col xs={12} md={6} lg={4} xl={3} key={category.idCategory}>
         <Card>
-          <Card.Img variant="top" src={category.strCategoryThumb} alt='category-img' />
+          <Link to="meal-list">
+            <Card.Img variant="top" src={category.strCategoryThumb} alt='category-img' />
+          </Link>
           <Card.Body>
-            <Card.Title>{category.strCategory}</Card.Title>
+            <Link to="meal-list">
+              <Card.Title className='category-title'><h2>{category.strCategory}</h2></Card.Title>
+            </Link>
             <OverlayTrigger
               trigger="click"
               key='bottom'
