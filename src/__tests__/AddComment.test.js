edc337b4ff804b76b71beb9de24d2cd4405c1b52
binyamyohannes <olangobinyam@gmail.com>
2024-01-08 06:59:55 +0300

    Add test for comment button

--- src/__tests__/AddComment.test.js
+++ src/__tests__/AddComment.test.js
@@ -58,4 +58,45 @@ describe("AddComment", () => {
     fireEvent.change(textInput, { target: { value: "Great one. I like it." } });
     expect(textInput.value).toBe("Great one. I like it.");
   })
+
+  test("Should the name of the comment button as Comment button clicked", async () => {
+    render(
+      <AddComment
+        mealID={1234}
+        updateComments={mockedUpdateComments}
+      />
+    );
+    const commentBtn = screen.getByRole('button', { name: "Comment" });
+    fireEvent.click(commentBtn)
+    const commentingBtn = screen.getByRole('button', { name: "Commenting" });
+    expect(commentingBtn).toBeInTheDocument();
+  })
+
+  // test("Should clear the input field as Comment button clicked", async () => {
+  //   render(
+  //     <AddComment
+  //       mealID={1234}
+  //       updateComments={mockedUpdateComments}
+  //     />
+  //   );
+  //   const nameInput = screen.getByPlaceholderText(/Your name/i);
+  //   const textInput = screen.getByPlaceholderText(/Your insights/i);
+  //   const commentBtn = screen.getByRole('button', { name: "Comment" });
+  //   fireEvent.change(nameInput, { target: { value: "Binyam Yohannes" } });
+  //   fireEvent.change(textInput, { target: { value: "Great one. I like it." } });
+  //   fireEvent.click(commentBtn)
+  //   expect(nameInput.value).toBe("");
+  //   expect(textInput.value).toBe("");
+  // })
+
+  // test("Should be able to submit as we click on comment button", async () => {
+  //   render(
+  //     <AddComment
+  //       mealID={1234}
+  //       updateComments={mockedUpdateComments}
+  //     />
+  //   );
+  //   const commentBtn = screen.getByRole('button', { name: "Comment" });
+  //   fireEvent.click(commentBtn)
+  // })
 })
